36bdb772e27b37541714bbc30e7aa30f6f420790
jdevenish <justin.devenish@gmail.com>
2020-05-05 16:55:36 -0400

    Missing s or corsheaders

--- Mockstagram_App/mockstagram/mockstagram/settings.py
+++ Mockstagram_App/mockstagram/mockstagram/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'rest_framework', # <= Added by user
     'apps.authentication', # <= Added by user
     'apps.api', # <= Added by user
-    'corsheader',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -57,14 +57,8 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True # If this is used then `CORS_ORIGIN_WHITELIST` will not have any effect
-# CORS_ALLOW_CREDENTIALS = True
-# CORS_ORIGIN_WHITELIST = [
-#     'http://localhost:3000',
-# ] # If this is used, then not need to use `CORS_ORIGIN_ALLOW_ALL = True`
-# CORS_ORIGIN_REGEX_WHITELIST = [
-#     'http://localhost:3000',
-# ]
+CORS_ORIGIN_ALLOW_ALL = True  # If this is used then `CORS_ORIGIN_WHITELIST` will not have any effect
+
 
 ROOT_URLCONF = 'mockstagram.urls'
 
