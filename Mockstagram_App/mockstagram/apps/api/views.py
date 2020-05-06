04513a5f68b4241f08d4db68be35b34346fa8278
jdevenish <justin.devenish@gmail.com>
2020-05-06 12:39:30 -0400

    logging

--- Mockstagram_App/mockstagram/apps/api/views.py
+++ Mockstagram_App/mockstagram/apps/api/views.py
@@ -18,7 +18,7 @@ class ProfileViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         print("print:ProfileVIewSet:getQuerySet")
-        print("self = ", self)
+        print("Profile.objects.all() = ", Profile.objects.all())
         return Profile.objects.all().filter(is_public=True)
 
     # Auth Required???
